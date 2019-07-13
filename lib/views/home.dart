df5276603958088cb670edef50743816588fe6e5
Hayder Ab <hayderux@gmail.com>
2019-07-13 17:09:47 +0400

    add windows

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,6 +2,7 @@ import 'package:apollo/style/xd.dart' as prefix0;
 import 'package:device_apps/device_apps.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:launcher_assist/launcher_assist.dart';
 
 class HomeView extends StatefulWidget {
@@ -15,12 +16,12 @@ class _HomeViewState extends State<HomeView> {
   @override
   void initState() {
     super.initState();
-    LauncherAssist.getWallpaper().then((imageData) {
+    /*LauncherAssist.getWallpaper().then((imageData) {
       setState(() {
         wallpaper = imageData;
       });
     });
-    getApps();
+    getApps();*/
   }
 
   void getApps() async {
@@ -35,15 +36,21 @@ class _HomeViewState extends State<HomeView> {
 
   Widget appContainer(BuildContext context, int index) {
     ApplicationWithIcon app = apps[index];
+
     return InkWell(
       onTap: () => DeviceApps.openApp(app.packageName),
       child: Column(
         children: <Widget>[
           Container(
-            height: 60,
-            width: 60,
-            decoration: BoxDecoration(shape: BoxShape.circle),
-            child: Image.memory(app.icon),
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey.shade200.withOpacity(0.4)),
+            child: Image.memory(
+              app.icon,
+            ),
           ),
         ],
       ),
@@ -55,23 +62,23 @@ class _HomeViewState extends State<HomeView> {
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
-              image: MemoryImage(wallpaper), fit: BoxFit.cover)),
+              image: AssetImage('assets/1.jpg'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0.0,
         ),
-        body: GridView.builder(
+        /*body: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 4,
-              childAspectRatio: 1.5,
-              crossAxisSpacing: 1.0,
-              mainAxisSpacing: 1),
+              childAspectRatio: 1.4,
+              crossAxisSpacing: 1.4,
+              mainAxisSpacing: 1.4),
           scrollDirection: Axis.horizontal,
           itemCount: apps.length,
           itemBuilder: appContainer,
-        ),
+        ),*/
         bottomNavigationBar: Container(
           height: 80,
           margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
@@ -80,8 +87,23 @@ class _HomeViewState extends State<HomeView> {
             boxShadow: prefix0.boxShadow,
             borderRadius: BorderRadius.circular(10),
           ),
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 10,
+            ),
+            child: Center(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(FontAwesomeIcons.windows),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
-
+}
